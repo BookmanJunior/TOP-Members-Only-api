44e34a3c35c12ee74440681acdadd6b4ba3979df
Azam Gulov <gulovazam@gmail.com>
2023-12-28 19:36:47 +0500

    Add format data virtual to MessageSchema

--- models/message.js
+++ models/message.js
@@ -1,9 +1,14 @@
 const { Schema, model } = require("mongoose");
+const { format } = require("date-fns");
 
 const MessageSchema = new Schema({
   message: { type: String, required: true },
   date: { type: Date, required: true },
   user: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+MessageSchema.virtual("formatted_date").get(function () {
+  return format(this.date, "MMM do h:mm a");
 });
 
 module.exports = model("Message", MessageSchema);
