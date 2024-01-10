3e5ea5a4b875a4f6a56e67fe742650277f96166a
Azam Gulov <gulovazam@gmail.com>
2024-01-10 20:06:36 +0500

    Refactor authorization middleware to use cookies instead of token bearer

--- auth/auth.js
+++ auth/auth.js
@@ -1,19 +1,19 @@
-const passport = require("passport");
-const { Strategy, ExtractJwt } = require("passport-jwt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-passport.use(
-  new Strategy(
-    {
-      secretOrKey: process.env.SECRET_TOKEN_KEY,
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    },
-    async (token, done) => {
-      try {
-        return done(null, token.user);
-      } catch (error) {
-        done(error);
-      }
-    }
-  )
-);
+exports.verifyJWT = async (req, res, next) => {
+  const token = req.cookies["jwt-token"];
+
+  if (!token) {
+    return res.sendStatus(403);
+  }
+
+  try {
+    const data = jwt.verify(token, process.env.SECRET_TOKEN_KEY);
+    req.userId = data._id;
+    req.username = data.username;
+    return next();
+  } catch (error) {
+    return res.sendStatus(403);
+  }
+};
