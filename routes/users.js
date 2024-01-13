63873790c99bc3d07cf53f002244ee233b74c236
Azam Gulov <gulovazam@gmail.com>
2024-01-13 23:28:08 +0500

    Move passport local strategy to loginController module

--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,7 @@
 var express = require("express");
 var router = express.Router();
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const User = require("../models/user");
+const loginController = require("../controllers/loginController");
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username }).exec();
-
-      if (!user) {
-        return done(null, false, { message: "Username not found." });
-      }
-
-      const isValidPassword = await user.validatePassword(password);
-
-      if (!isValidPassword) {
-        return done(null, false, { message: "Incorrect password." });
-      }
-
-      return done(null, user);
-    } catch (error) {
-      return done(error);
-    }
-  })
-);
-
-passport.serializeUser((user, done) => {
-  return done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id).exec();
-    return done(null, user);
-  } catch (error) {
-    return done(error);
-  }
-});
-
-router.post(
-  "/",
-  passport.authenticate("local", {
-    successRedirect: "/message-board",
-    failureRedirect: "/",
-    failureFlash: true,
-  })
-);
+router.post("/", loginController.login_post);
 
 module.exports = router;
