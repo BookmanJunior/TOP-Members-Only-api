dcce0093683963e23978b7a6a4edfcfac58dd32d
Azam Gulov <gulovazam@gmail.com>
2024-01-18 20:55:50 +0500

    Change delete message route

--- routes/message-board.js
+++ routes/message-board.js
@@ -6,6 +6,6 @@ router.get("/", messageController.messages_get);
 
 router.post("/", messageController.message_post);
 
-router.post("/delete-message", messageController.delete_message_post);
+router.post("/:messageId/delete", messageController.delete_message_post);
 
 module.exports = router;
